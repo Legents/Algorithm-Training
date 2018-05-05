ef8289e12f671426f54433365d3ef9588820f9ca
Legents <li_guannan@163.com>
2018-05-05 22:04:02 +0800

    --

--- 多项式合并.cpp
+++ 多项式合并.cpp
@@ -1,51 +1,98 @@
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct Node {
-	int a1, a2, a3;//分别表示每一项的系数
+	int a1, a2;//分别表示系数和幂数
 	struct Node *next;
 }Node,*LinkList;
-void InitList(LinkList *h)//初始化链表
+LinkList InitList()//初始化链表
 {
-	*h = (LinkList)malloc(sizeof(Node));
+	LinkList h;
+	h = (LinkList)malloc(sizeof(Node));
 	if (!h)//判断是否为h分配到所需空间
 	{
 		printf("初始化链表错误！\n");
 		exit(0);
 	}
-	(*h)->next = NULL;
+	h->next = NULL;
+	return h;
 }
-void InsertList(LinkList *h, int a1, int a2, int a3)//将输入的系数插入到当前链表中 {
-{	LinkList s;
-	s = (LinkList)malloc(sizeof(Node));
-	s->a1 = a1;
-	s->a2 = a2;
-	s->a3 = a3;
-	s->next = (*h)->next;
-	(*h)->next = s;
+void InsertList(LinkList h, int a1, int a2)//将输入的系数插入到当前链表中 
+{	
+	
+		LinkList p, s;
+		s = (LinkList)malloc(sizeof(Node));
+		s->a1 = a1;
+		s->a2 = a2;
+		p = h;
+		s->next = p->next;
+		p->next = s;
+}
+/*LinkList InsertSort(LinkList h) {
+	if (h == NULL || h->next == NULL) {
+		return h;
+	}
+	LinkList p = h->next;
+	while (p) {
+		if (p->a2 < h->a2) {
+			LinkList temp1 = h;
+			while (temp1->next != p) {
+				temp1 = temp1->next;
+			}
+			LinkList temp = p;
+			p = p->next;
+			temp1->next = p;
+			temp->next = h;
+			h = temp;
+			continue;
+		}
+		LinkList q = h;
+		while (q->next != p) {
+			if (q->next->a2 < p->a2) {
+				q = q->next;
+			}
+			else
+				break;
+		}
+		if (q->next == p) {
+			p = p->next;
+		}
+		else {
+			LinkList temp1 = q;
+			while (temp1->next != q) {
+				temp1 = temp1->next;
+			}
+			LinkList temp = p;
+			p = p->next;
+			temp1->next = p;
+			temp->next = q->next;
+			q->next = temp;
+		}
+	}
+	return h;
+}*/
+void Print(LinkList h) {
+	LinkList p = h->next;
+	while (p) {
+		printf("%d %d\n", p->a1,p->a2);
+		p = p->next;
+	}
+	printf("\n");
 }
 int main() {
 	LinkList h;
-	InitList(&h);
-	int n,a1,a2,a3,i,sum1=0,sum2=0,sum3=0;
-	printf("输入你要合并的多项式的个数:\n");
-	scanf("%d", &n);
-	printf("请分别输入二次项，一次项以及常数项的系数:\n");
-	for (i = 0; i < n; i++) {
-		scanf("%d%d%d", &a1, &a2, &a3);
-		InsertList(&h, a1, a2, a3);
-	}
-	LinkList p;
-	p = (LinkList)malloc(sizeof(Node));
-	p = h->next;
-	while (p)//分别对各项系数求和
-	{
-		sum1 += p->a1;
-		sum2 += p->a2;
-		sum3 += p->a3;
-		p = p->next;
-	}
+	h=InitList();
+	int a1,a2;
+	printf("分别输入每一项的系数和幂数，以0，0结束：\n");
+	do {
+		scanf("%d %d", &a1, &a2);
+		InsertList(h, a1, a2);
+	}while (a1 != 0 || a2 != 0);
+	Print(h);
+	LinkList s;
+	s = InitList();
+	s= InsertSort(h);
+	Print(s);
 	printf("合并后的多项式为：\n");
-	printf("%dX^2+%dX+%d\n", sum1, sum2, sum3);
 	system("pause");
 	return 0;
 }
